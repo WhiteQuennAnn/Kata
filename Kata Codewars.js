b3861267639f207504ffd33f1d9816d6ecb62357
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-10-22 19:35:59 +0300

    22102022

--- Kata Codewars.js
+++ Kata Codewars.js
@@ -1,9 +1,3 @@
-function century(year) {
-let result = 0;
-for (let i = 0; i < year; i++) {
-if (i % 100 === 0) {
-result++;
-}
-}
-return result;
+function repeatStr (n, s) {
+    return s.repeat(n);
 }
