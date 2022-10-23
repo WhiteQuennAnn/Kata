f8751198da18bdc9a421d6bf32fd73b0e0938177
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-10-23 19:39:31 +0300

    23102022

--- Kata Codewars.js
+++ Kata Codewars.js
@@ -1,9 +1,7 @@
-function abbrevName(name){
-    const answer = [name[0]]
-    for(let i = 0; i < name.length; i++) {
-        if(name[i] === " ") {
-            answer.push(name[i + 1]);
-        }
+function countSheeps(arrayOfSheep) {
+    let counter = 0
+    for(let i = 0; i < arrayOfSheep.length; i++) {
+        if(arrayOfSheep[i]) counter += 1
     }
-    return answer.map(el => el.toUpperCase()).join(".")
+    return counter
 }
