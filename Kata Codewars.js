46850e67921d8dfd3f7641e7f0c8d75d043f09b3
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-10-25 17:20:00 +0300

    25102022

--- Kata Codewars.js
+++ Kata Codewars.js
@@ -1,3 +1,9 @@
 function check(a, x) {
-    return a.includes(x) ? true : false;
+    return a.includes(x) ? true : falsfunction past(h, m, s){
+        //#Happy Coding! ^_^
+        const convertHours = h * 60 * 60 * 1000;
+        const convertMinutes = m * 60 * 1000;
+        const convertSeconds = s * 1000;
+        return convertHours + convertMinutes + convertSeconds;
+    };
 }
