16f5b8c82aa5f4171a677ee2774e8bd622847426
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-10-25 17:05:53 +0300

    25102022

--- Kata Codewars.js
+++ Kata Codewars.js
@@ -1,7 +1,3 @@
-var summation = function (num) {
-    let sum = 0
-    for(let i = 0; i <= num; i++) {
-        sum =sum + i
-    }
-    return sum
+function findNeedle(haystack) {
+    return "found the needle at position " + haystack.indexOf("needle");
 }
