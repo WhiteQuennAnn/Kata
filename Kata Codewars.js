da1bc462a9a33b31cf6c2d006bef1e4513ee2efd
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-10-25 16:57:06 +0300

    25102022

--- Kata Codewars.js
+++ Kata Codewars.js
@@ -1,15 +1,7 @@
-function countPositivesSumNegatives(input) {
-    if (input === null || input.length < 1) {
-        return [];
+var summation = function (num) {
+    let sum = 0
+    for(let i = 0; i <= num; i++) {
+        sum += i
     }
-    var array = [0, 0];
-
-    for(var i = 0; i < input.length; i++) {
-        if(input[i] <= 0) {
-            array[1] += input[i];
-        } else {
-            array[0] += 1;
-        }
-    }
-    return array;
+    return sum
 }
